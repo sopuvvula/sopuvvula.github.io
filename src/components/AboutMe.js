f84dcaf96185e98e7bc4f6af8e5621f02ad87591
Mohan Dasari <mohan@Ultron>
2023-08-02 10:59:25 -0400

    Setup project tree, added logo, invis header, and background

---
+++ src/components/AboutMe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>I was born and raised in Southern California...</p>
+      <p>In my free time, I enjoy playing League of Legends, working out, and trying new foods.</p>
+    </div>
+  );
+};
+
+export default AboutMe;
